2d1462b62a5ed8d88116c87c1d542978cb102000
OKURA Masafumi <masafumi.o1988@gmail.com>
2021-04-09 22:14:42 +0900

    [Feat] Add `define_hook` method

--- lib/tiny_hooks.rb
+++ lib/tiny_hooks.rb
@@ -1,8 +1,58 @@
 # frozen_string_literal: true
 
-require_relative "tiny_hooks/version"
+require_relative 'tiny_hooks/version'
 
+# TinyHooks is the gem to easily define hooks.
+# `extend` this module and now you can define hooks with `define_hook` method.
+# See the test file for more detailed usage.
 module TinyHooks
   class Error < StandardError; end
-  # Your code goes here...
+
+  # rubocop:disable Metrics/MethodLength
+  # Define hook with kind and target method
+  #
+  # @param [Symbol, String] kind the kind of the hook, possible values are: :before, :after and :around
+  # @param [Symbol, String] target the name of the targeted method
+  def define_hook(kind, target, &block)
+    original_method = instance_method(target)
+    body = case kind.to_sym
+           when :before
+             proc do |*args, **kwargs, &blk|
+               instance_exec(*args, **kwargs, &block)
+               original_method.bind_call(self, *args, **kwargs, &blk)
+             end
+           when :after
+             proc do |*args, **kwargs, &blk|
+               original_method.bind_call(self, *args, **kwargs, &blk)
+               instance_exec(*args, **kwargs, &block)
+             end
+           when :around
+             proc do |*args, **kwargs, &blk|
+               wrapper = -> { original_method.bind_call(self, *args, **kwargs, &blk) }
+               instance_exec(wrapper, *args, **kwargs, &block)
+             end
+           else
+             raise Error, "#{kind} is not supported."
+           end
+    undef_method(target)
+    define_method(target, &body)
+  end
+  # rubocop:enable Metrics/MethodLength
+
+  module_function :define_hook
+
+  # Restore original method from the registry
+  #
+  # @param [Symbol, String] target the name of the method to restore
+  def restore_original(target)
+    original = registry.fetch(target.to_sym) { instance_method(target) }
+    undef_method(target)
+    define_method(target, original)
+  end
+
+  private
+
+  def registry
+    @registry ||= {}
+  end
 end
--- test/tiny_hooks_test.rb
+++ test/tiny_hooks_test.rb
@@ -1,13 +1,101 @@
 # frozen_string_literal: true
 
-require "test_helper"
+require 'test_helper'
 
 class TinyHooksTest < Minitest::Test
+  class C
+    extend TinyHooks
+
+    def a
+      puts 'a'
+    end
+  end
+
   def test_that_it_has_a_version_number
     refute_nil ::TinyHooks::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  class C1 < C
+    define_hook :before, :a do
+      puts 'before a'
+    end
+  end
+
+  class C2 < C
+    define_hook :after, :a do
+      puts 'after a'
+    end
+  end
+
+  class C3 < C
+    define_hook :around, :a do |a|
+      puts 'before a'
+      a.call
+      puts 'after a'
+    end
+  end
+
+  class C4 < C
+    define_hook :before, :a do
+      puts 'before a 1'
+    end
+
+    define_hook :before, :a do
+      puts 'before a 2'
+    end
+  end
+
+  class C5 < C
+    define_hook :after, :a do
+      puts 'after a 1'
+    end
+
+    define_hook :after, :a do
+      puts 'after a 2'
+    end
+  end
+
+  class C6 < C
+    define_hook :around, :a do |a|
+      puts 'before a 1'
+      a.call
+      puts 'after a 1'
+    end
+
+    define_hook :around, :a do |a|
+      puts 'before a 2'
+      a.call
+      puts 'after a 2'
+    end
+  end
+
+  def test_it_defines_before_hook
+    c = C1.new
+    assert_output("before a\na\n") { c.a }
+  end
+
+  def test_it_defines_after_hook
+    c = C2.new
+    assert_output("a\nafter a\n") { c.a }
+  end
+
+  def test_it_defines_around_hook
+    c = C3.new
+    assert_output("before a\na\nafter a\n") { c.a }
+  end
+
+  def test_it_defines_before_hook_twice
+    c = C4.new
+    assert_output("before a 2\nbefore a 1\na\n") { c.a }
+  end
+
+  def test_it_defines_after_hook_twice
+    c = C5.new
+    assert_output("a\nafter a 1\nafter a 2\n") { c.a }
+  end
+
+  def test_it_defines_around_hook_twice
+    c = C6.new
+    assert_output("before a 2\nbefore a 1\na\nafter a 1\nafter a 2\n") { c.a }
   end
 end
