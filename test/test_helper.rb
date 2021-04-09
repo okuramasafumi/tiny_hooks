f0bb3ef17f35d322587cab76ea99360570e418d6
OKURA Masafumi <masafumi.o1988@gmail.com>
2021-04-09 17:55:57 +0900

    [Lint] autocorrect test_helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-$LOAD_PATH.unshift File.expand_path("../lib", __dir__)
-require "tiny_hooks"
+$LOAD_PATH.unshift File.expand_path('../lib', __dir__)
+require 'tiny_hooks'
 
-require "minitest/autorun"
+require 'minitest/autorun'
